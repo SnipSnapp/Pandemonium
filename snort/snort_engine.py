15018b915eb03c4dc1de8661e9b2ce91d549d8a6
SnipSnap <70385098+SnipSnapp@users.noreply.github.com>
2023-01-22 20:20:51 -0600

    Update snort_engine.py

--- snort/snort_engine.py
+++ snort/snort_engine.py
@@ -12,12 +12,13 @@ class Snort_Engine():
     def __init__(self, rule_folder:str, config:str):
         self.rules = {}
         self.config = config
+
         self.rule_files = os.listdir(rule_folder)
         if not rule_folder.endswith('/'):
             rule_folder = rule_folder + '/'
         for count,fname in enumerate(self.rule_files):
             self.rule_files[count] = rule_folder + fname
-        print("parsing rules...")
+        #print("parsing rules...")
         self.__parse_rule_files__()
         self.rules_list = list(self.rules.keys())
         self.__select_rule__()
@@ -32,16 +33,19 @@ class Snort_Engine():
                     f.close
                 matches = re.findall(self.RULE_MATCH,rulez)
                 for x in matches:
-                    rl = Snort_Rule(''.join(x),self.config)
-                    increment = 0
-                    rname = rl.return_name()
-                    while rname in self.rules:
-                        increment +=1
-                        rname = rl.return_name() +'-'+ str(increment)
+                    try:
+                        rl = Snort_Rule(''.join(x),self.config)
+                        increment = 0
+                        rname = rl.return_name()
+                        while rname in self.rules:
+                            increment +=1
+                            rname = rl.return_name() +'-'+ str(increment)
 
-                    self.rules.update({rname:rl})
-                    print("parsed rule: " + rname)
-                    #self.rules.update({x:Snort_Rule(''.join(x),self.config)})
+                        self.rules.update({rname:rl})
+                        #print("parsed rule: " + rname)
+                        #self.rules.update({x:Snort_Rule(''.join(x),self.config)})
+                    except:
+                        pass
                 print("parsed rule file: " + rule_f)
             else:
                 print("Snort rule file path DNE: " + rule_f)
@@ -52,16 +56,37 @@ class Snort_Engine():
         self.play_pcap(self.rules)
 
     def play_pcap(self, rule):
+        print(self.rules.keys())
         
-        rule_definition = self.rules['bugtraq,57842'].rules
+        #rule_definition = self.rules['bugtraq,57842'].rules
         print(self.rules['bugtraq,57842'].rules[0])
-        print("rule iz")
-        print(rule_definition[1])
-        print('\n')
+        #print("rule iz")
+        #print(rule_definition[1])
+        #print('\n')
+        playone=True
+        item_todo =0
+        my_keys = list(self.rules.keys())
+        print(type(my_keys))
+        for itemno in my_keys[item_todo:]:
+            
+            if playone:
+                print(self.rules[itemno].rules[0])
+                print(self.rules[itemno].rules[1])
+                for x in range(5):
+                    print(f'\n\n{self.rules[itemno].rules[0]}\n\n{self.rules[itemno].rules[1]}\n\n')
+                    traffic_player(self.rules[itemno].rules[0],self.rules[itemno].rules[1],None,None).send_traffic()
 
-        traffic_player(self.rules['bugtraq,57842'].rules[0],self.rules['bugtraq,57842'].rules[1],None,None).send_traffic()
-        
-        
+              
+                 
+                print(my_keys[item_todo+1])
+                playone = False
+
+            else:
+                
+                exit
+                
+           
+        print("done...")
         exit(0)
 
     
