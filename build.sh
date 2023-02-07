dbbe75b1916db58775948c6d6f9aebbdeada71a2
SnipSnap <70385098+SnipSnapp@users.noreply.github.com>
2023-02-06 22:05:44 -0600

    Update build.sh
    forgot the -perl

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 apt-get install python3 -y
 apt-get install pip -y
 pip3 install -r requirements.txt
-apt-get install libstring-random -y
-apt-get install libfile-slurper -y
+apt-get install libstring-random-perl -y
+apt-get install libfile-slurper-perl -y
